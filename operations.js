54664d3fcffff96d1be918dcd15db44947ef4dc6
peri <periahmadi@gmail.com>
2021-04-14 23:16:49 -0700

    new kata

---
+++ operations.js
@@ -0,0 +1,19 @@
+function basicOp(operation, value1, value2)
+{
+  let add = value1 + value2
+  let subtract = value1-value2
+  let multiply = value1*value2
+  let divide = value1/value2
+  if (operation === '+'){
+      return add
+      }
+  else if (operation === '-'){
+    return subtract
+  }else if (operation === '*'){
+            return multiply
+  }else if (operation === '/'){
+            return divide
+            }
+
+  // Code
+}
